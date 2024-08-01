8b26abfcbdfc8d5402de5bfe91592e07222704b3
ashwinn-si <ashwin23cse@gmail.com>
2024-08-02 00:01:57 +0530

    LANDING PAGE DONE..!

---
+++ script.js
@@ -0,0 +1,12 @@
+if(window.innerWidth<768){
+    alert("kindly view the all the webistes in desktop for better experience");
+}
+document.getElementById("T1").addEventListener("click",()=>{
+    window.location.href="TASK-1[Calculator]/index.html";
+})
+document.getElementById("T2").addEventListener("click",()=>{
+    window.location.href="TASK-2[Currency-Converter]/index.html";
+})
+document.getElementById("T3").addEventListener("click",()=>{
+    window.location.href="TASK-3[Login-Page]/index.html";
+})
